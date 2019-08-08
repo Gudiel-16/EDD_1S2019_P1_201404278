11da7c27bfaf69cb804f8e16ac8b58b911d44482
Gudiel-16 <53126441+Gudiel-16@users.noreply.github.com>
2019-08-08 05:40:55 -0600

    Version 4
    Lista Doble Snake, Creacion de bocadillos.

--- practica1.py
+++ practica1.py
@@ -2,6 +2,7 @@ import curses
 import time
 import csv
 import random
+import os
 
 
 from curses import KEY_RIGHT, KEY_LEFT, KEY_UP, KEY_DOWN, textpad
@@ -56,9 +57,60 @@ class dobleCircularUsuarios():
         for i in range(index):
             actual=actual.siguiente
         return actual.nombre
-            
 
-listaDobleCircularUsuarios=dobleCircularUsuarios() #objeto como variable global  
+""" ----------------------------------------------LISTA DOBLE PARA SNAKE ----------------------------------------------"""
+class nodoDobleSnake():
+
+    def __init__(self, coordenadas):                  
+        self.siguiente = None        
+        self.anterior =  None
+        self.coordenadas = coordenadas 
+
+class dobleSnake():
+    def __init__(self):
+        self.primero=None
+        self.ultimo=None
+        self.size=-1
+
+    def estaVacia(self):
+        return self.primero is None
+    
+    def insertarInicio(self, coordenadas):
+        nuevo=nodoDobleSnake(coordenadas)
+        if self.estaVacia():
+            self.primero=nuevo
+            self.ultimo=nuevo
+        else:
+            nuevo.siguiente=self.primero
+            self.primero.anterior=nuevo
+            self.primero=nuevo
+
+        self.size=self.size+1
+
+    def insertarFinal(self,coordenadas):
+        nuevo=nodoDobleSnake(coordenadas)
+        if self.estaVacia():
+            self.primero=nuevo
+            self.ultimo=nuevo
+        else:
+            self.ultimo.siguiente=nuevo
+            nuevo.anterior=self.ultimo
+            self.ultimo=nuevo
+            
+        self.size=self.size+1
+
+    def imprimirLista(self):
+    
+        if self.estaVacia():
+            print("lista Vacia")
+        else:            
+            temp=self.primero
+            for i in range(self.size+1):
+                print(temp.coordenadas,end=" ")
+                temp=temp.siguiente
+
+listaDobleCircularUsuarios=dobleCircularUsuarios() #objeto como variable global
+listaDobleSnake=dobleSnake()  
 
 """ ----------------------------------------------PARA EL MENU PRINCIPAL ----------------------------------------------"""
 def print_menu(stdscr, selected_row_idx):
@@ -109,8 +161,19 @@ def menu_principal(stdscr):
         print_menu(stdscr,indice_fila_actual)
         stdscr.refresh()
 
-""" ----------------------------------------------JUGAR ----------------------------------------------"""
 
+""" ----------------------------------------------PARA LA COMIDA Y  ----------------------------------------------"""
+def crear_comida(snake):
+    comidaa=None
+
+    while comidaa is None:
+        comidaa=[random.randint(3, 19),random.randint(3,75)]
+        if comidaa in snake:
+            comidaa=None
+    return comidaa
+
+
+""" ----------------------------------------------JUGAR ----------------------------------------------"""
 def jugar():
     stdscr=curses.initscr()
     sh, sw=stdscr.getmaxyx()
@@ -124,80 +187,103 @@ def jugar():
     window.nodelay(True)
 
     key=KEY_RIGHT
-    pos_X=5
-    pos_Y=7
     
-
-    snake=[[pos_Y, 8],[pos_Y, 7],[pos_Y, 6]]#tmanio snake
+    snake=[[7, 8],[7, 7],[7, 6]]#tamanio snake
 
     for y,x in snake:
-        window.addch(y,x,'*')
-        #stdscr.addstr(y,x,'@')
+        window.addch(y,x,'@')
+    
+    comida=crear_comida(snake)
+    window.addch(comida[0], comida[1], '+')
 
-    window.addch(23,78,'#')
+    comidapop=crear_comida(snake)
+    window.addch(comidapop[0], comidapop[1], '*') 
+    
+    
 
     while key!=27:
-        window.timeout(100)
+        window.timeout(120)
         keystroke= window.getch()
 
         cabeza=snake[0]
 
         if keystroke is not -1:
-            key=keystroke
-                
-        window.addch(pos_Y,pos_X,' ')
-        """
-        if pos_X is 1:
-            pos_X=39
-            window.border(0)
-        elif pos_X is 39:
-            pos_X=1
-            window.border(0)
-        
-        if pos_Y is 0:
-            pos_Y=19
-        elif pos_Y is 19:
-            pos_Y=0
-        """
+            key=keystroke                
+       
         if key==KEY_RIGHT:
             #pos_X=pos_X es como poner pausa
             nuevaCabeza=[cabeza[0], cabeza[1]+1]
         elif key==KEY_LEFT:
-            #pos_X=pos_X-1
             nuevaCabeza=[cabeza[0], cabeza[1]-1]
         elif key==KEY_UP:
-            #pos_Y=pos_Y-1
             nuevaCabeza=[cabeza[0]-1, cabeza[1]]
         elif key==KEY_DOWN:
-            #pos_Y=pos_Y+1
             nuevaCabeza=[cabeza[0]+1, cabeza[1]]
+
         snake.insert(0,nuevaCabeza)
         window.addch(nuevaCabeza[0], nuevaCabeza[1],'@')
-        window.addch(snake[-1][0], snake[-1][1], ' ')
+
+        if snake[0] ==comida:
+            comida=crear_comida(snake)
+            window.addch(comida[0], comida[1], '+')
+            
+        elif snake[0] ==comidapop:
+            comidapop=crear_comida(snake)
+            window.addch(comidapop[0], comidapop[1], '*')
+            aa=[snake.pop()] #captura coordenadas del pop()            
+            for x,y in aa:
+                window.addch(x, y, ' ') #pinta nada en las coordenadas del pop()
+            bb=[snake.pop()]
+            for x,y in bb:
+                window.addch(x, y, ' ')    
+            #score+=1
+            #print_score(stdscr,score)
+        else:
+            window.addch(snake[-1][0], snake[-1][1], ' ')
+            snake.pop()
+          
         
         indiceIzquierdo=[[1,1],[2,1],[3,1],[4,1],[5,1],[6,1],[7,1],[8,1],[9,1],[10,1],[11,1],[12,1],[13,1],[14,1],[15,1],[16,1],[17,1],[18,1],[19,1],[20,1],[21,1],[22,1],[23,1]]
         indiceDerecho=[[1,78],[2,78],[3,78],[4,78],[5,78],[6,78],[7,78],[8,78],[9,78],[10,78],[11,78],[12,78],[13,78],[14,78],[15,78],[16,78],[17,78],[18,78],[19,78],[20,78],[21,78],[22,78],[23,78]]
-
+        indiceSuperior=[[1,2],[1,3],[1,4],[1,5],[1,6],[1,7],[1,8],[1,9],[1,10],[1,11],[1,12],[1,13],[1,14],[1,15],[1,16],[1,17],[1,18],[1,19],[1,20],[1,21],[1,22],[1,23],[1,24],[1,25],[1,26],[1,27],[1,28],[1,29],[1,30],[1,31],[1,32],[1,33],[1,34],[1,35],[1,36],[1,37],[1,38],[1,39],[1,40],[1,41],[1,42],[1,43],[1,44],[1,45],[1,46],[1,47],[1,48],[1,49],[1,50],[1,51],[1,52],[1,53],[1,54],[1,55],[1,56],[1,57],[1,58],[1,59],[1,60],[1,61],[1,62],[1,63],[1,64],[1,65],[1,66],[1,67],[1,68],[1,69],[1,70],[1,71],[1,72],[1,73],[1,74],[1,75],[1,76],[1,77]]
+        indiceInferior=[[23,2],[23,3],[23,4],[23,5],[23,6],[23,7],[23,8],[23,9],[23,10],[23,11],[23,12],[23,13],[23,14],[23,15],[23,16],[23,17],[23,18],[23,19],[23,20],[23,21],[23,22],[23,23],[23,24],[23,25],[23,26],[23,27],[23,28],[23,29],[23,30],[23,31],[23,32],[23,33],[23,34],[23,35],[23,36],[23,37],[23,38],[23,39],[23,40],[23,41],[23,42],[23,43],[23,44],[23,45],[23,46],[23,47],[23,48],[23,49],[23,50],[23,51],[23,52],[23,53],[23,54],[23,55],[23,56],[23,57],[23,58],[23,59],[23,60],[23,61],[23,62],[23,63],[23,64],[23,65],[23,66],[23,67],[23,68],[23,69],[23,70],[23,71],[23,72],[23,73],[23,74],[23,75],[23,76],[23,77]]
+        
         if (snake[0] in indiceIzquierdo[0:]):  
             nuevaCabeza=[cabeza[0], cabeza[1]+76]
             snake.insert(0,nuevaCabeza)
             window.addch(nuevaCabeza[0], nuevaCabeza[1],'@')
-            window.addch(snake[-1][0], snake[-1][1], ' ')
         elif (snake[0] in indiceDerecho[0:]): 
             nuevaCabeza=[cabeza[0], cabeza[1]-76]
             snake.insert(0,nuevaCabeza)
             window.addch(nuevaCabeza[0], nuevaCabeza[1],'@')
             window.addch(snake[-1][0], snake[-1][1], ' ')
+        elif (snake[0] in indiceSuperior[0:]):
+            nuevaCabeza=[cabeza[0]+21, cabeza[1]]
+            snake.insert(0,nuevaCabeza)
+            window.addch(nuevaCabeza[0], nuevaCabeza[1],'@')
+            window.addch(snake[-1][0], snake[-1][1], ' ')
+        elif (snake[0] in indiceInferior[0:]):
+            nuevaCabeza=[cabeza[0]-21, cabeza[1]]
+            snake.insert(0,nuevaCabeza)
+            window.addch(nuevaCabeza[0], nuevaCabeza[1],'@')
+            window.addch(snake[-1][0], snake[-1][1], ' ')
         elif (snake[0] in snake[1:]): 
+            for y,x in snake[1:]:
+                coord=str(x) + "," + str(y)
+                print(y,x)
+                listaDobleSnake.insertarFinal(coord)
+            msg="HAS PERDIDO VUELVE A INTENTARLO!"
+            stdscr.addstr(sh//2, sw//2 - len(msg)//2, msg)
+            stdscr.nodelay(0)
+            stdscr.getch()
             break
 
-        snake.pop()
 
         window.refresh()
-        #window.addch(pos_Y,pos_X,'*')
 
     curses.endwin()
 
+'''    
 def crear_comida(snake, box):
     comida=None
 
@@ -207,7 +293,7 @@ def crear_comida(snake, box):
         if comida in snake:
             comida=None
     return comida
- 
+''' 
 
 def print_score(stdscr, score):
     sh,sw=stdscr.getmaxyx()
@@ -286,8 +372,6 @@ def jugar2(stdscr):
 
         stdscr.refresh()
 
-        
-
 """ ----------------------------------------------CARGA MASIVA ----------------------------------------------"""
 
 def cargaMasiva(): 
@@ -345,6 +429,36 @@ def pintar_menu(stdsrc, index):
     stdsrc.refresh()
     # -----------------------------------------------------------
 
+""" ----------------------------------------------GRAFICAR SNAKE -----------------------------------------------------"""    
+def graficarSnake():
+    # open(nombre_archivo.ext, formato)
+    f = open("otro2.dot", "w") 
+    # write("texto a escribir") 
+    f.write("digraph G {\n")
+    f.write("node [shape = square];\n")
+    f.write("A") # SU ESTRUCTURA!!!!, PILA, COLA, LISTA_DOBLE, CIRCULAR
+    
+    snake=[[2, 8],[2, 7],[2, 6]]#tmanio snake    
+    for y,x in snake:
+        coord=str(x) + "," + str(y)
+        print(coord)
+
+    f.write("}")
+    # CIERRA EL ARCHIVO
+    f.close()
+    # dot -Tjpg ruta_archivo_dot.dot -o nombre_archivo_salida.jpg
+    os.system("dot -Tjpg"+ " otro2.dot " +"-o imagen2.jpg")
+    os.system("imagen2.jpg")
+
+def aa():
+    aa=""
+    for i in range(77):
+        aja="["+str(23)+","+str(i)+"],"
+        aa+=aja
+    print(aa)
+
+#aa()
+#graficarSnake()
 jugar()
 #curses.wrapper(jugar2)
 #curses.wrapper(menu_principal)
